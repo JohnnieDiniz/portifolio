7ca2a662e6206dd2e2d05f9d14f70d6cca1812b7
Johnnie_Diniz <diniz.vitor.alves@gmail.com>
2026-02-08 00:10:45 -0300

    primeiro commit

---
+++ cadastro_livros.py
@@ -0,0 +1,20 @@
+'''
+Docstring for cadastro_livros
+Funcionalidades obrigatórias
+Cadastrar livros (título e autor)
+Listar todos os livros cadastrados
+Buscar livros pelo título
+Menu simples:
+1 – Cadastrar livro
+2 – Listar livros
+3 – Buscar livro
+0 – Sair
+Conceitos praticados
+Entrada de dados (input)
+Listas
+Dicionários ou classes
+Estruturas de repetição (while)
+Condicionais (if / elif / else)
+'''
+
+
